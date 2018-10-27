4b4e67ccd05a55b0b6e31dd6d7966a8037acf597
James Goldie <me@rensa.co>
2018-10-27 13:59:54 +1100

    Initial commit (reports working, colouring not)

---
+++ R/pillar.r
@@ -0,0 +1,49 @@
+# report_*_color: add coloring to the report outputs.
+# used by the pillar_shaft() functions
+
+report_safely_color = function(x) {
+
+  # colour present components
+  x = sub('R', crayon::green('R'), x, fixed = TRUE)
+  x = sub('E', crayon::red('E'), x, fixed = TRUE)
+
+  # colour all absent components grey
+  x = gsub('_', crayon::gray('_'), x, fixed = TRUE)
+  format(x, justify = "left")
+}
+
+report_quietly_color = function(x) {
+
+  # orange should fall back without 256 colours
+  crayon_orange = crayon::make_style('orange')
+
+  # colour present components
+  x = sub('R', crayon::green('R'), x, fixed = TRUE)
+  x = sub('W', crayon_orange('W'), x, fixed = TRUE)
+  x = sub('M', crayon::yellow('M'), x, fixed = TRUE)
+  x = sub('O', crayon::white('O'), x, fixed = TRUE)
+
+  # colour all absent components grey
+  x = gsub('_', crayon::gray('_'), x, fixed = TRUE)
+  format(x, justify = "left")
+}
+
+
+#' @importFrom pillar pillar_shaft
+#' @export
+pillar_shaft.report_safely <- function(x, ...) {
+  out <- format(x, formatter = report_safely_color)
+  # out[is.na(x)] <- NA
+  pillar::new_pillar_shaft_simple(out, align = "left",
+    width = 2, min_width = 2, na_indent = 0)
+}
+
+#' @importFrom pillar pillar_shaft
+#' @export
+pillar_shaft.report_quietly <- function(x, ...) {
+  out <- format(x, formatter = report_quietly_color)
+  # out[is.na(x)] <- NA
+  pillar::new_pillar_shaft_simple(out, align = "left",
+    width = 4, min_width = 4, na_indent = 0)
+}
+
---
+++ test/tests.r
@@ -0,0 +1,19 @@
+safe_log = safely(log)
+quiet_log = quietly(log)
+
+test =
+  # tidy up and trim down for the example
+  mtcars %>%
+  tibble::rownames_to_column(var = "car") %>%
+  tibble::as_data_frame() %>%
+  dplyr::select(car, cyl, disp, wt) %>%
+  # spike some rows in cyl == 4 to make them fail
+  dplyr::mutate(wt = dplyr::case_when(
+    wt < 2 ~ -wt,
+    TRUE ~ wt)) %>%
+  # nest and do some operations quietly()
+  tidyr::nest(-cyl) %>%
+  dplyr::mutate(
+    qlog = map(data, ~ quiet_log(.$wt)),
+    qlog_status = report_quietly(qlog))
+
